eb6737db3f556d0aa6ab42b075bf4405656530ba
Alejandro Hernandez <alhernandez1593@gmail.com>
2019-01-04 13:28:05 -0600

    generating random url

--- app/models/url_shortener.rb
+++ app/models/url_shortener.rb
@@ -1,3 +1,9 @@
 class UrlShortener < ApplicationRecord
+  after_initialize :create_url_short
   validates_presences_of :original_url
+
+  def create_url_short
+    self.visited = 0
+    self.shorter_url = ([*('A'..'Z'),*('0'..'9')]-%w(0 1 O)).sample(6).join
+  end
 end
