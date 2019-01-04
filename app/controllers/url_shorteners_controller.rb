3c4fd74cde1f8c109a699d2ee532161d9d382926
Alejandro Hernandez <alhernandez1593@gmail.com>
2019-01-04 16:00:01 -0600

    improvments prevent duplicates domains and url shortener tokens

--- app/controllers/url_shorteners_controller.rb
+++ app/controllers/url_shorteners_controller.rb
@@ -1,8 +1,12 @@
 class UrlShortenersController < ApplicationController
+  before_action :find_url, only: [:create]
+
   def create
-    @url_shortener = UrlShortener.new(url_shortener_params)
-    @url_shortener.save
-    render json: @url_shortener.shorter_url
+    unless @url_shortener.nil? 
+      @url_shortener = UrlShortener.new(url_shortener_params)
+      @url_shortener.save
+    end
+    render json: request.base_url + "/" + @url_shortener.shorter_url
   end
 
   def top_hundred_visited
@@ -12,14 +16,18 @@ class UrlShortenersController < ApplicationController
 
   def redirect_to_original
     @url_shortener = UrlShortener.find_by(shorter_url: params[:shorter_url])
+    @url_shortener.increment!(:visited)
     redirect_to @url_shortener.original_url
   end
 
   private
 
   def url_shortener_params
-    params.permit(:original_url)
+    params.permit(:original_url, :visited)
   end
 
+  def find_url
+    @url_shortener = UrlShortener.find_by(original_url: url_shortener_params[:original_url])
+  end
 
 end
--- app/models/url_shortener.rb
+++ app/models/url_shortener.rb
@@ -1,9 +1,9 @@
 class UrlShortener < ApplicationRecord
-  after_initialize :create_url_short
+  before_create :create_url_short
   validates_presence_of :original_url
 
   def create_url_short
     self.visited = 0
-    self.shorter_url = ([*('A'..'Z'), *('0'..'9')] - %w[0 1 O]).sample(6).join
+    self.shorter_url = ([*('A'..'Z'), *('0'..'9')] - %w[0 1 O]).sample(6).join until UrlShortener.find_by_shorter_url(self.shorter_url).nil?
   end
 end
